fd83a0d362b77780b55e375a1a2eafd7116922de
Leigh Oliver <leigh@leigholiver.com>
2024-11-04 14:06:01 +0000

    fix(ecs): fix pagination issue syncing ECS tasks (#3)
    * fix(ecs): fix pagination issue syncing ECS tasks
    
    * chore: version bump

--- ae/aws/ecs.py
+++ ae/aws/ecs.py
@@ -5,74 +5,72 @@ from . import session
 from .. import shell
 
 def describe_tasks(role=None):
-
     client = session.client("ecs", role)
-    cluster_arns = client.list_clusters()
     tasks = []
-    for cluster in cluster_arns["clusterArns"]:
-        task_arns = client.list_tasks(
-            cluster=cluster,
-            desiredStatus="RUNNING"
-        )
 
-        if task_arns["taskArns"] == []:
-            continue
+    # Handle pagination for clusters
+    paginator = client.get_paginator('list_clusters')
+    for cluster_page in paginator.paginate():
+        for cluster in cluster_page["clusterArns"]:
+            # Handle pagination for tasks
+            tasks_paginator = client.get_paginator('list_tasks')
+            for task_page in tasks_paginator.paginate(
+                cluster=cluster,
+                desiredStatus="RUNNING"
+            ):
+                if not task_page["taskArns"]:
+                    continue
 
-        response = client.describe_tasks(
-            cluster=cluster,
-            tasks=task_arns["taskArns"],
-            include=["TAGS"]
-        )
-
-        for t in response["tasks"]:
-            if t["lastStatus"] != "RUNNING":
-                continue
-
-            t["tags"] = { tag["key"]: tag["value"] for tag in t["tags"] } if "tags" in t.keys() else {}
-
-            # we can get these from ecs managed tags, but only works on EC2 launch type
-            # to rely on them later on, we set the expected tags
-            cluster_name = "".join(t["clusterArn"].split("/")[1:])
-            service_name = t['group'].replace("service:", "")
-            t['tags']['aws:ecs:clusterName'] = cluster_name
-            t['tags']['aws:ecs:serviceName'] = service_name
-
-            t["Name"] = service_name
-            t["Role"] = role
-            t["Kind"] = "ecs"
-            t["Id"] = t["taskArn"].split("/")[-1]
-
-            if len(t['containers']) == 1:
-                t["Ident"] = resources.build_ident(
-                    t,
-                    unique_id=t["Id"],
-                    role=role
-                )
-
-                # You can use this to use some of the EC2 SSM functions - https://stackoverflow.com/a/67641633
-                if 'runtimeId' in t['containers'][0].keys():
-                    t["InstanceId"] = f"ecs:{cluster_name}_{t['Id']}_{t['containers'][0]['runtimeId']}"
-                tasks.append(t)
-
-            else:
-                for c in t['containers']:
-                    # clone the task so that we can create a resource-per-container
-                    current = t.copy()
-
-                    # remove the other containers
-                    current["containers"] = [c]
-
-                    current["Ident"] = f"{c['name']}-"
-                    current["Ident"] += resources.build_ident(
-                        current,
-                        unique_id=current["Id"],
-                        role=role
+                # describe_tasks API has a limit of 100 tasks per call
+                for task_arns_chunk in [task_page["taskArns"][i:i+100] for i in range(0, len(task_page["taskArns"]), 100)]:
+                    response = client.describe_tasks(
+                        cluster=cluster,
+                        tasks=task_arns_chunk,
+                        include=["TAGS"]
                     )
 
-                    # You can use this to use some of the EC2 SSM functions - https://stackoverflow.com/a/67641633
-                    if 'runtimeId' in c.keys():
-                        current["InstanceId"] = f"ecs:{cluster_name}_{current['Id']}_{c['runtimeId']}"
-                    tasks.append(current)
+                    for t in response["tasks"]:
+                        if t["lastStatus"] != "RUNNING":
+                            continue
+
+                        t["tags"] = { tag["key"]: tag["value"] for tag in t["tags"] } if "tags" in t.keys() else {}
+                        # we can get these from ecs managed tags, but only works on EC2 launch type
+                        # to rely on them later on, we set the expected tags
+                        cluster_name = "".join(t["clusterArn"].split("/")[1:])
+                        service_name = t['group'].replace("service:", "")
+                        t['tags']['aws:ecs:clusterName'] = cluster_name
+                        t['tags']['aws:ecs:serviceName'] = service_name
+                        t["Name"] = service_name
+                        t["Role"] = role
+                        t["Kind"] = "ecs"
+                        t["Id"] = t["taskArn"].split("/")[-1]
+
+                        if len(t['containers']) == 1:
+                            t["Ident"] = resources.build_ident(
+                                t,
+                                unique_id=t["Id"],
+                                role=role
+                            )
+                            # You can use this to use some of the EC2 SSM functions - https://stackoverflow.com/a/67641633
+                            if 'runtimeId' in t['containers'][0].keys():
+                                t["InstanceId"] = f"ecs:{cluster_name}_{t['Id']}_{t['containers'][0]['runtimeId']}"
+                            tasks.append(t)
+                        else:
+                            for c in t['containers']:
+                                # clone the task so that we can create a resource-per-container
+                                current = t.copy()
+                                # remove the other containers
+                                current["containers"] = [c]
+                                current["Ident"] = f"{c['name']}-"
+                                current["Ident"] += resources.build_ident(
+                                    current,
+                                    unique_id=current["Id"],
+                                    role=role
+                                )
+                                # You can use this to use some of the EC2 SSM functions - https://stackoverflow.com/a/67641633
+                                if 'runtimeId' in c.keys():
+                                    current["InstanceId"] = f"ecs:{cluster_name}_{current['Id']}_{c['runtimeId']}"
+                                tasks.append(current)
 
     return tasks
 
